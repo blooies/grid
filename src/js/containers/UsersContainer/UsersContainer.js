cba31a8e8184e0f0b910cecb9b824aebd92a7c38
blooies <joy28@msn.com>
2017-11-30 23:51:07 -0500

    add structure of users container

--- src/js/containers/UsersContainer/UsersContainer.js
+++ src/js/containers/UsersContainer/UsersContainer.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import SortFilter from '../../components/SortFilter/SortFilter';
+import CategoryFilter from '../../components/CategoryFilter/CategoryFilter';
+import UsersGrid from '../../components/UsersGrid/UsersGrid';
+import { loadUsers } from '../../actions/users';
+import getUnique from '../../utils/getUnique';
+import './users-container.less';
 
+// main app
 class UsersContainer extends Component {
   constructor(props) {
     super(props);
@@ -10,13 +17,44 @@ class UsersContainer extends Component {
     }
   }
 
+  componentDidMount() {
+    const { loadUsers } = this.props;
+    loadUsers();
+  }
+
+  handleCategorySelect = (e) => {
+    const currentCategory = e.target.value;
+    this.setState({
+      currentCategory
+    });
+  }
+
+  handleSortToggle = (e) => {
+    const currentSort = e.target.value;
+    this.setState({
+      currentSort
+    })
+  }
+
   render() {
     const { currentCategory, currentSort } = this.state;
     const { users } = this.props;
+    const categories = getUnique(users.map(user => user.category));
 
     return (
       <div className='main-container'>
         <h2>Users Grid</h2>
+        <div className='main-container__filter'>
+          <SortFilter/>
+          <CategoryFilter
+            currentCategory={currentCategory}
+            categories={categories}
+            handleCategorySelect={this.handleCategorySelect}
+          />
+        </div>
+        <UsersGrid
+          users={users}
+        />
       </div>
     );
   }
@@ -24,10 +62,13 @@ class UsersContainer extends Component {
 
 
 const mapStateToProps = (state) => {
+  // const { users, sortType, filterType } = state;
   const { users } = state;
 
   return {
     users
+    // sortType,
+    // filterType
   }
 };
 
