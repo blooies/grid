a88aa645aaa4e50b4251f16335ecde4d7d42cc37
blooies <joy28@msn.com>
2017-12-02 00:55:35 -0500

    clean up and refactor users actions

--- src/js/actions/users.js
+++ src/js/actions/users.js
@@ -1,21 +1,14 @@
-// GET USERS 
-function fetchUsers() {
-  return dispatch => {
-    return fetch('/data')
-      .then(response => response.json())
-      .then(users => dispatch(usersSuccess(users.users)))
-  }
-}
-
 export function usersSuccess(users) {
   return {
     type: 'USERS_SUCCESS',
     users
-  };
+  }
 }
 
 export function loadUsers() {
   return (dispatch, getState) => {
-    return dispatch(fetchUsers());
+    return fetch('/data')
+      .then(response => response.json())
+      .then(users => dispatch(usersSuccess(users.users)))
   }
 }
