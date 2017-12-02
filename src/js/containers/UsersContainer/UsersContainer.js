c8647a2d60a75c92c4ee003ee10b4543f9c8569a
blooies <joy28@msn.com>
2017-12-02 01:00:14 -0500

    add sorting and filtering logic to users container

--- src/js/containers/UsersContainer/UsersContainer.js
+++ src/js/containers/UsersContainer/UsersContainer.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { sortBy, uniq } from 'underscore';
 import SortFilter from '../../components/SortFilter/SortFilter';
 import CategoryFilter from '../../components/CategoryFilter/CategoryFilter';
 import UsersGrid from '../../components/UsersGrid/UsersGrid';
 import { loadUsers } from '../../actions/users';
-import getUnique from '../../utils/getUnique';
-import './users-container.less';
+import { setCategoryFilter } from '../../actions/filterType';
+import { setSort } from '../../actions/sortType';
 
-// main app
 class UsersContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentSort: 'featured',
+      currentSort: '',
       currentCategory: ''
     }
   }
@@ -23,60 +23,88 @@ class UsersContainer extends Component {
   }
 
   handleCategorySelect = (e) => {
-    const currentCategory = e.target.value;
-    this.setState({
-      currentCategory
-    });
+    const value = e.target.value === 'all' ? '' : e.target.value;
+    const { setCategoryFilter } = this.props;
+    setCategoryFilter(value);
   }
 
   handleSortToggle = (e) => {
-    const currentSort = e.target.value;
-    this.setState({
-      currentSort
-    })
+    const value = e.target.value === 'featured' ? '' : e.target.value;;
+    const { setSort } = this.props;
+    setSort(value);
+  }
+
+  sortUsers(users) {
+    const { sortType } = this.props;
+    switch (sortType) {
+      case 'ascending':
+        return sortBy(users, 'name');
+        break;
+      case 'descending':
+        return sortBy(users, 'name').reverse();
+        break;
+      case 'priority':
+        return sortBy(users, 'priority');
+        break;
+      default:
+        return users;
+    }
+  }
+
+  filterUsers(users) {
+    const { filterType } = this.props;
+    return users.filter(user => user.category === filterType);
+  }
+
+  getVisibleUsers() {
+    const { users, filterType, sortType } = this.props;
+    const sortedUsers = sortType ? this.sortUsers(users) : users;
+    const filteredUsers = filterType ? this.filterUsers(sortedUsers) : sortedUsers;
+    return filteredUsers;
   }
 
   render() {
-    const { currentCategory, currentSort } = this.state;
-    const { users } = this.props;
-    const categories = getUnique(users.map(user => user.category));
+    const { users, sortType, filterType } = this.props;
+    const categories = uniq(users.map(user => user.category));
+    const visibleUsers = this.getVisibleUsers();
 
     return (
       <div className='main-container'>
         <h2>Users Grid</h2>
         <div className='main-container__filter'>
-          <SortFilter/>
+          <SortFilter
+            handleSortToggle={this.handleSortToggle}
+          />
           <CategoryFilter
-            currentCategory={currentCategory}
+            currentCategory={filterType}
             categories={categories}
             handleCategorySelect={this.handleCategorySelect}
           />
         </div>
         <UsersGrid
-          users={users}
+          users={visibleUsers}
         />
       </div>
     );
   }
 }
 
-
 const mapStateToProps = (state) => {
-  // const { users, sortType, filterType } = state;
-  const { users } = state;
+  const { users, filterType, sortType } = state;
 
   return {
-    users
-    // sortType,
-    // filterType
+    users,
+    filterType,
+    sortType
   }
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    loadUsers: (users) => dispatch(loadUsers(users))
+    loadUsers: (users) => dispatch(loadUsers(users)),
+    setCategoryFilter: (filterType) => dispatch(setCategoryFilter(filterType)),
+    setSort: (sortType) => dispatch(setSort(sortType))
   }
 };
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
