e2e8e7a0db84edaf1c53e4fe49610c30d16a873c
blooies <joy28@msn.com>
2017-12-02 00:58:57 -0500

    add sort and filter reducers

---
+++ src/js/reducers/filterType.js
@@ -0,0 +1,8 @@
+export function filterType(state = '', action) {
+  switch (action.type) {
+    case 'SET_CATEGORY_FILTER':
+      return action.filterType
+    default:
+      return state
+  }
+}
---
+++ src/js/reducers/sortType.js
@@ -0,0 +1,8 @@
+export function sortType(state = '', action) {
+  switch (action.type) {
+    case 'SET_SORT':
+      return action.sortType
+    default:
+      return state
+  }
+}
